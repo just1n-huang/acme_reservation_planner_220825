a0a74aa5f9af5ce68b1e3fc0903974a0e2f1261f
Justin Huang <justin88@gmail.com>
2022-08-26 20:32:01 -0400

    restaurant list complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
+const { fetchJSON } = require("./apiHelpers");
+
+let restaurants;
+let users;
+
 // webpack looks in src first for index.js
 // index.js holds the client side code
 
-console.log("hello world!!");
+const userList = document.querySelector("#users-list");
+const restaurantsList = document.querySelector("#restaurants-list");
+
+const setup = async () => {
+  // console.log("starting");
+  // fetch is a web api
+  // a restful api
+  users = await fetchJSON("/api/users");
+  const htmlUsers = users
+    .map((user) => {
+      return `
+    <li>${user.name}</li>
+    `;
+    })
+    .join("");
+  userList.innerHTML = htmlUsers;
+
+  restaurants = await fetchJSON("/api/restaurants");
+
+  const htmlRestaurants = restaurants
+    .map((restaurant) => {
+      return `
+      <li>
+        ${restaurant.name}
+      </li>
+    `;
+    })
+    .join("");
+  restaurantsList.innerHTML = htmlRestaurants;
+  console.log(htmlRestaurants);
+};
+
+setup();
