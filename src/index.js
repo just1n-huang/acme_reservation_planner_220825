6ce1934fa0b9cdad0202ac697a7c92b11e7bf8a1
Justin Huang <justin88@gmail.com>
2022-08-27 01:09:56 -0400

    current

--- src/index.js
+++ src/index.js
@@ -2,72 +2,107 @@ const { fetchJSON } = require("./apiHelpers");
 
 let restaurants;
 let users;
+let reservations = [];
 
-// webpack looks in src first for index.js
-// index.js holds the client side code
+const usersList = document.querySelector("#users-list");
 
-const userList = document.querySelector("#users-list");
 const restaurantsList = document.querySelector("#restaurants-list");
+const reservationsList = document.querySelector("#reservations-list");
 
 const renderUsers = () => {
   const id = window.location.hash.slice(1);
   const html = users
     .map((user) => {
-      return ` 
-      <li class =${id * 1 === user.id ? "selected" : ""}>
-        <a href ='#${user.id}'> 
+      return `
+      <li class=${id * 1 === user.id ? "selected" : ""}>
+        <a href='#${user.id}'>
         ${user.name}
         </a>
       </li>
     `;
     })
     .join("");
-  userList.innerHTML = html;
+  usersList.innerHTML = html;
+};
+
+const renderReservations = () => {
+  const html = reservations
+    .map((reservation) => {
+      const restaurant = restaurants.find(
+        (restaurant) => restaurant.id === reservation.restaurantId
+      );
+      return `
+      <li>
+        ${restaurant.name}
+      </li>
+    `;
+    })
+    .join("");
+  reservationsList.innerHTML = html;
 };
 
 const renderRestaurants = () => {
   const html = restaurants
     .map((restaurant) => {
-      const count = reservations.filter(
+      const count = (reservations || []).filter(
         (reservation) => reservation.restaurantId === restaurant.id
       ).length;
-
       return `
-    <li>
-      ${restaurant.name} (${count})
-    </li>
-  `;
+      <li data-id='${restaurant.id}'>
+        ${restaurant.name} (${count})
+      </li>
+    `;
     })
     .join("");
   restaurantsList.innerHTML = html;
 };
 
 const setup = async () => {
-  // console.log("starting");
-  // fetch is a restful web api
-
   users = await fetchJSON("/api/users");
-  // separate out renderUser data and call it
   renderUsers();
 
   const id = window.location.hash.slice(1);
   if (id) {
     reservations = await fetchJSON(`/api/users/${id}/reservations`);
   }
-  // console.log(reservations);
 
   restaurants = await fetchJSON("/api/restaurants");
-  // separate out renderRestaurant data and call it
+  renderReservations();
   renderRestaurants();
 };
 
 setup();
 
-// notes
-// in a client side application you will end up getting and rendering that data
-// separate out the getting the data and rendering the data
+restaurantsList.addEventListener("click", async (ev) => {
+  const id = window.location.hash.slice(1);
+  if (!id) {
+    return;
+  }
 
-window.addEventListener("hashchange", () => {
+  if (ev.target.tagName === "LI") {
+    const restaurantId = ev.target.getAttribute("data-id");
+    const response = await fetch(`/api/users/${id}/reservations`, {
+      method: "POST",
+      body: JSON.stringify({ restaurantId }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const reservation = await response.json();
+    reservations.push(reservation);
+    renderReservations();
+    renderRestaurants();
+  }
+});
+
+window.addEventListener("hashchange", async () => {
   renderUsers();
+  const id = window.location.hash.slice(1);
+  if (id) {
+    reservations = await fetchJSON(`/api/users/${id}/reservations`);
+  } else {
+    reservations = [];
+  }
   renderRestaurants();
+  renderReservations();
 });
